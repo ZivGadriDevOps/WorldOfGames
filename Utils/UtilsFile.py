d889a35b17ffd535bf30f490c3c198bbacdcdf59
ZivGadriDevOps <zivgadri@gmail.com>
2022-02-25 11:28:19 +0200

    docker-compose.yml

--- Utils/UtilsFile.py
+++ Utils/UtilsFile.py
@@ -1,4 +1,4 @@
-SCORES_FILE_PATH = "Resources/scores.txt"
+SCORES_FILE_PATH = "../Resources/scores.txt"
 CHROME_DRIVER_FILE_PATH = "../Utils/chromedriver.exe"
 APP_URL = "http://192.168.0.107:5000/score/ziv"
 BAD_RETURN_CODE = -1
