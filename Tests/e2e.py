cfe285eac8d567bc2a147b3818c13566cc151709
ZivGadriDevOps <zivgadri@gmail.com>
2022-03-01 12:08:07 +0200

    Jenkinsfile and docker-compose - update

--- Tests/e2e.py
+++ Tests/e2e.py
@@ -3,4 +3,7 @@ from TestHelper import main_function
 
 APP_URL = "http://localhost:5000/score/ziv"
 
-main_function(APP_URL)
+try:
+    main_function(APP_URL)
+except FileNotFoundError as fnf_err:
+    print("File not found. More Info:\n %s" % fnf_err.with_traceback())
